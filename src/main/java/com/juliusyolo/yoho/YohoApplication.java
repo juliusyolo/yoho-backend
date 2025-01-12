ee7c413d9cb2807881dac4208f0ab407cd90cc1d
juliusyolo <xiaoshengsheng1994@gmail.com>
2025-01-12 23:33:25 +0800

    rag configuration

--- src/main/java/com/juliusyolo/yoho/YohoApplication.java
+++ src/main/java/com/juliusyolo/yoho/YohoApplication.java
@@ -3,11 +3,13 @@ package com.juliusyolo.yoho;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableWebFlux
+@EnableMethodSecurity
 public class YohoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/juliusyolo/yoho/controller/ChatController.java
+++ src/main/java/com/juliusyolo/yoho/controller/ChatController.java
@@ -1,18 +1,18 @@
 package com.juliusyolo.yoho.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class ChatController {
 
-    @GetMapping("/api/v1/ai/test")
-    public String test(){
-        return "test";
-    }
-
-    @GetMapping("/api/v1/ai/chat1")
-    public String chat(){
-        return "test";
+    @GetMapping("/api/v1/ai/chat")
+    public Mono<String> chat(){
+        //TODO
+       return ReactiveSecurityContextHolder.getContext().map(SecurityContext::getAuthentication).map(Authentication::getName);
     }
 }
